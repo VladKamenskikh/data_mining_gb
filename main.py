dcbad1edf2bd793b1efe38730496daf808494cfa
VladKamenskikh <vkamenskikh0890@gmail.com>
2021-05-06 12:37:40 +0500

    Lesson 3 HW

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from autoyoula.spiders.youla import YoulaSpider
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule('autoyoula.settings')
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(YoulaSpider)
+    crawler_process.start()
