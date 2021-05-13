eeb0a101acab61cf48115e62ff2d5a8e93e4da17
VladKamenskikh <vkamenskikh0890@gmail.com>
2021-05-13 11:38:23 +0500

    Lesson 6 HW

--- avito/loaders.py
+++ avito/loaders.py
@@ -8,22 +8,39 @@ def get_title(itm):
 
 
 def get_url(itm):
+<<<<<<< HEAD
     return urljoin("https://www.avito.ru", itm)
 
 
 def get_price(itm):
     itm = itm.replace(" ", "")
+=======
+    return urljoin('https://www.avito.ru', itm)
+
+
+def get_price(itm):
+    itm = itm.replace(' ', '')
+>>>>>>> 92657dc (Lesson 6 HW)
     return float(itm)
 
 
 def get_address(itm):
+<<<<<<< HEAD
     itm = itm.replace("\n ", "")
+=======
+    itm = itm.replace('\n ', '')
+>>>>>>> 92657dc (Lesson 6 HW)
     return itm
 
 
 def get_parameters(data):
+<<<<<<< HEAD
     parameters_list_raw = [itm.replace(":", "") for itm in data if (itm != " ") and (itm != "\n  ")]
     parameters_list = [itm[:-1] if itm[-1] == " " else itm for itm in parameters_list_raw]
+=======
+    parameters_list_raw = [itm.replace(':', '').replace('\xa0', '') for itm in data if (itm != ' ') and (itm != '\n  ')]
+    parameters_list = [itm[:-1] if itm[-1] == ' ' else itm for itm in parameters_list_raw]
+>>>>>>> 92657dc (Lesson 6 HW)
     parameters_dict = dict(zip(parameters_list[::2], parameters_list[1::2]))
     return parameters_dict
 
